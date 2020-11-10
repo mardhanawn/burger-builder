4fd30882cc2046364a777bb270717e9b3475561e
Ardhana <mardhanawn@gmail.com>
2020-11-10 09:40:03 +0700

    send POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
@@ -83,7 +84,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert(' You continue!');
+        // alert(' You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'John Trump Biden',
+                address: {
+                    street: 'Grand Surya',
+                    zipCode: '123344',
+                    region: 'Sidoarjo'
+                },
+                email: 'john@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
