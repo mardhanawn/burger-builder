7628d2a0c67d5a16211966e3ae96859d5c48366c
Ardhana <mardhanawn@gmail.com>
2020-11-12 09:02:00 +0700

    fetch orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
-const order = (props) => {
+const order = (props) => (
     <div className={classes.Order}>
         <p>Ingredients : {props.ingredients} </p>
         <p><strong>Price : Rp. {props.price} </strong></p>
     </div>
-}
+);
 
 export default order; 
