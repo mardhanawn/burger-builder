bf5d190be250f1f928023d7febe804a0ae65400c
Ardhana <mardhanawn@gmail.com>
2020-11-12 16:57:50 +0700

    add dropdown component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -12,13 +12,26 @@ const input = (props) => {
                 {...props.elementConfig} 
                 value={props.value} />;
             break;
-            case('textarea'):
+        case('textarea'):
             inputElement = <textarea 
                 className={classes.InputElement}
                 {...props.elementConfig} 
                 value={props.value} />;
             break;
-            default:
+        case('select'):
+            inputElement = (
+                <select 
+                    className={classes.InputElement}
+                    value={props.value}>
+                        {props.elementConfig.options.map(option => (
+                            <option key={option.value} value={option.value}>
+                                {option.displayValue}
+                            </option>
+                        ))}
+                </select>   
+            );
+            break;
+        default:
             inputElement = <input 
                 className={classes.InputElement} 
                 {...props.elementConfig} 
