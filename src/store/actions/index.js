f86b2ff463f89f6b14b2e70eb692289b10ed511e
Ardhana <mardhanawn@gmail.com>
2020-11-17 15:39:56 +0700

    connect contact data container & actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,4 +4,8 @@ export {
     initIngredients
 } from './burgerBuilder';
 
-export {} from './order';
+export {
+    purchaseBurgerSuccess,
+    purchaseBurgerFail,
+    purchaseBurgerStart
+} from './order';
