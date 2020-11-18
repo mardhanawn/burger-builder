d000969bb2d4999bb5fd121a6b81e200bdaa9472
Ardhana <mardhanawn@gmail.com>
2020-11-18 15:32:20 +0700

    add sign in auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBgERW6LcEKtAPi_vjJzEtIOauxMDNhcIc', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBgERW6LcEKtAPi_vjJzEtIOauxMDNhcIc';
+        if (!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBgERW6LcEKtAPi_vjJzEtIOauxMDNhcIc';
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response)
                 dispatch(authSuccess(response.data));
