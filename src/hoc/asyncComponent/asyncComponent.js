726bb60f9709558fd3e88581038f17b1d06feab3
Ardhana <mardhanawn@gmail.com>
2020-11-19 14:55:38 +0700

    add lazy loading

---
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const asyncComponent = (importComponent) => {
+    return class extends Component {
+        componentDidMount () {
+            importComponent()
+                .then(cmp => {
+                    this.setState({component: cmp.default});
+                });
+        }
+        
+        render() {
+            const C = this.state.component;
+    
+            return C ? <C {...this.props} /> : null;
+        }
+    }
+}
+
+export default asyncComponent;
