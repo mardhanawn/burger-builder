3d1770edd94b7e8354da5dd7a7efc38c31ac29e6
Ardhana <mardhanawn@gmail.com>
2020-11-19 15:02:34 +0700

    fix asyncComponent lazy loading

--- src/hoc/asyncComponent/asyncComponent.js
+++ src/hoc/asyncComponent/asyncComponent.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 const asyncComponent = (importComponent) => {
     return class extends Component {
+        state = {
+            component: null
+        }
+
         componentDidMount () {
             importComponent()
                 .then(cmp => {
